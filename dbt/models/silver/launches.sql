3514ea60759fa8b8c87ae3f3374b4923a2f2d550
mcanroe <mcanroe21@gmail.com>
2025-05-10 23:26:11 +0100

    feat: add initial silver models and clean up naming

---
+++ dbt/models/silver/launches.sql
@@ -0,0 +1,24 @@
+{{ config(materialized='table') }}
+
+select 
+    id as launch_id,
+    "name",
+    launch_designator,
+    status__id as status_id,
+    status__abbrev as "status",
+	status__description as "description",
+    net at time zone 'Europe/London' as launch_dttm_bst, -- convert UTC to BST
+    image__thumbnail_url as image_thumbnail_url,
+    image__credit as image_credit,
+    failreason as fail_reason,
+    rocket__id as rocket_id,
+    rocket__configuration__full_name as rocket_configuration_name,
+    mission__id as mission_id,
+    mission__name as mission_name,
+    mission__type as mission_type,
+    mission__description as mission_description,
+    mission__orbit__id as mission_orbit_id,
+    mission__orbit__name as mission_orbit_name,
+    pad__id as pad_id,
+    last_updated at time zone 'Europe/London' as last_updated_bst,
+from {{ source('bronze', 'launches') }}
---
+++ dbt/models/silver/pads.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+select
+    id as launch_id,
+    pad__id as pad_id,
+    pad__name as pad_name,
+    pad__image__name as pad_image_name,
+    pad__image__thumbnail_url as pad_image_thumbnail_url,
+    pad__image__credit as pad_image_credit,
+    pad__description as pad_description,
+    pad__wiki_url as pad_wiki_url,
+    pad__map_url as pad_map_url,
+    pad__total_launch_count as pad_total_launch_count,
+    pad__location__name as pad_location_name,
+    pad__location__description as pad_location_description,
+    pad__location__map_image as pad_location_map_image,
+    pad__location__image__credit as pad_location_image_credit,
+from {{ source('bronze', 'launches') }}
